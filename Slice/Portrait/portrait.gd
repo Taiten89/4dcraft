8efeac7ed59cb33ba0d9fd0ec8e2e588594b3b5c
Taiten <taiten@example.com>
2026-06-07 10:43:36 +0200

    Slice/Portrait: Add a "fore_margin" that places the viewer behind the rendered range.

--- Slice/Portrait/portrait.gd
+++ Slice/Portrait/portrait.gd
@@ -1,24 +1,26 @@
 class_name Portrait
 extends RefCounted
 
-var max_distance: float
+var fore_margin := 0.5
+var fore_range := 2.0
+var max_distance := fore_margin + fore_range
 var slices: Array[Slice] = []
 var distances: Array[float] = []
 
 
-func _init (mid: Slice, n: int, new_max_distance := 2.0) -> void:
-	max_distance = new_max_distance
+func _init (mid: Slice, n: int) -> void:
 	slices.resize(n)
 	distances.resize(n)
 
 	if n == 1:
-		slices[0] = make_slice(mid, 1.0)
+		var distance := fore_margin + fore_range/2
+		slices[0] = make_slice(mid, distance)
 		return
 
 	for i in range(n):
-		var i_n := float(i) / float(n)
-		var distance_rev := max_distance * i_n ** 3
-		var distance := max_distance - distance_rev
+		var i_n := float(i) / float(n-1)
+		var mapped_i := fore_range * i_n ** 3
+		var distance := fore_margin + fore_range - mapped_i
 		slices[i] = make_slice(mid, distance)
 		distances[i] = distance
 
@@ -26,9 +28,10 @@ func _init (mid: Slice, n: int, new_max_distance := 2.0) -> void:
 func make_slice (mid: Slice, distance: float) -> Slice:
 	var slice := mid.copy()
 
-	var fore := slice.base.column(1)
-	slice.position.translate_inplace(-fore)
-	slice.position.translate_inplace(distance * fore)
+	var fore_factor := distance - fore_range/2 - fore_margin
+	var fore_column := slice.base.column(1)
+	slice.position.translate_inplace(fore_factor * fore_column)
+
 	slice.base.scale_inplace(distance / 2)
 
 	return slice
